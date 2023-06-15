8e17f0ea810f2e356603a45d6211c56e1a039be5
Nishant Vashishth <gyansahoo666@gmail.com>
2023-06-15 17:12:31 +0530

    Updating test

--- src/test/java/com/browserstack/run_first_test/FirstTest.java
+++ src/test/java/com/browserstack/run_first_test/FirstTest.java
@@ -419,11 +419,11 @@ public class FirstTest extends BrowserStackTestNGTest {
     Thread.sleep(1000);
     GetElement(wait,1,ShopName,"android.widget.TextView","").click();
 
-    WebElement hereNow= GetElement(wait,0,"Are you here now?","android.widget.TextView","");
-    hereNow.click();
-
-    WebElement YesButton= GetElement(wait,0,"yes","android.widget.Button","");
-    YesButton.click();
+//    WebElement hereNow= GetElement(wait,0,"Are you here now?","android.widget.TextView","");
+//    hereNow.click();
+//
+//    WebElement YesButton= GetElement(wait,0,"yes","android.widget.Button","");
+//    YesButton.click();
     Thread.sleep(1000);
 
 }
