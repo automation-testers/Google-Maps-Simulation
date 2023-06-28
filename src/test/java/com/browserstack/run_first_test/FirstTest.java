9e90979ce13595b21936f6bfae4ff88d935f7614
Automation Tester <119865696+automation-testers@users.noreply.github.com>
2023-06-28 11:48:38 -0700

    Update FirstTest.java

--- src/test/java/com/browserstack/run_first_test/FirstTest.java
+++ src/test/java/com/browserstack/run_first_test/FirstTest.java
@@ -112,9 +112,9 @@ public class FirstTest extends BrowserStackTestNGTest {
             System.out.println("Catch block 1 ended");
         }
         waitt.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View/android.widget.EditText")));
-        try{
         WebElement EIdPassword = FindEle(wait, com.browserstack.run_first_test.FirstTest.SearchBy.ByXPath,"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View/android.widget.EditText");
-        EIdPassword.sendKeys(Parts[1]);
+        //EIdPassword.sendKeys(Parts[1]);
+        EIdPassword.sendKeys("70143w".03cdijj1679(#)A3062zFM8222");
         Thread.sleep(2000);
         System.out.println("Password Entered");
         waitt.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[3]/android.view.View/android.widget.Button")));
@@ -122,19 +122,7 @@ public class FirstTest extends BrowserStackTestNGTest {
         PSNextButton.click();
         System.out.println("Next Button Clicked");
         Thread.sleep(4000);
-        } catch (Exception E) {
-            System.out.println("Catch block 1");
-            EIdPassword.sendKeys("70143w".03cdijj1679(#)A3062zFM8222");
-            Thread.sleep(2000);
-            System.out.println("Hard coded Password Entered");
-            waitt.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[3]/android.view.View/android.widget.Button")));
-            WebElement PSNextButton = FindEle(wait, com.browserstack.run_first_test.FirstTest.SearchBy.ByXPath,"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[3]/android.view.View/android.widget.Button");
-            PSNextButton.click();
-            System.out.println("Next Button Clicked");
-            Thread.sleep(4000);
-        }
                 
-        
 //        waitt.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("")));
         WebElement EnterAuthCodeLabel = GetElement(wait,0,"Get a verification code from the Google Authenticator app@Check your","android.widget.TextView","");
         if(EnterAuthCodeLabel!=null)
